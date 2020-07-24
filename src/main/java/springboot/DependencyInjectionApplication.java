38a458c2fb20a1580cc37936719676018dc2d3fb
Adina Dumitru <adina.eni@gmail.com>
2020-07-24 07:38:14 +0000

    lecture 24

--- src/main/java/springboot/DependencyInjectionApplication.java
+++ src/main/java/springboot/DependencyInjectionApplication.java
@@ -5,10 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 // If declared beans are not in the same package as this starter class
-@ComponentScan("guru.springframework")
+//@ComponentScan("guru.springframework")
+
+// Used with .xml files spring configuration
+@ImportResource("classpath:/spring/spring-config.xml")
 public class DependencyInjectionApplication {
 
     public static void main(String[] args) {
