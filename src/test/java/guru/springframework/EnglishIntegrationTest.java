7c5e250c7c51f666c69feacb143a961262426864
Adina Dumitru <adina.eni@gmail.com>
2020-07-29 08:25:38 +0000

    lecture 26

---
+++ src/test/java/guru/springframework/EnglishIntegrationTest.java
@@ -0,0 +1,26 @@
+package guru.springframework;
+
+import guru.springframework.services.HelloWorldService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/spring/helloworld-config.xml",
+        "classpath*:/spring/english-hello-world.xml"})
+public class EnglishIntegrationTest {
+
+    @Autowired
+    HelloWorldService helloWorldService;
+
+    @Test
+    public void testHelloWorld() {
+        String greeting = helloWorldService.getGreeting();
+
+        assertEquals("Hello World!", greeting);
+    }
+}
